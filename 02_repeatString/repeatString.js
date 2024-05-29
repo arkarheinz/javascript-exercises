8f09d96fbca537afdd25515d141a2d07814f0579
Arkar Hein <arkarhein14314@gmail.com>
2024-05-30 00:19:40 +0700

    Commit repeatString exercise
    [02_repeatString]

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    string = 'hey';
+    num = 0;
+    for (num = 1; num <=3; num++) {
+        return string + string + string;
+    }
+    console.log(repeatString);
 };
 
 // Do not edit below this line
