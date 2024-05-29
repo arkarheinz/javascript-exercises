52d266227e6a2dabe0d451600e6554eaeb10443c
Arkar Hein <arkarhein14314@gmail.com>
2024-05-30 00:37:51 +0700

    Commit reverse String exercise
    [03_reverseString]

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // change string into array
+    const arrayString = string.split('');
 
+    // reverse array
+    const reverseArray = arrayString.reverse();
+
+    // join array to become one word
+    const reverseString = reverseArray.join('');
+
+    return reverseString;
 };
+
+console.log(reverseString('hello there'));
 
 // Do not edit below this line
 module.exports = reverseString;
