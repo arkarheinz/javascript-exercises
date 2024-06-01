dc1cf0dea76c3b6a44c946c5164f49ec02958890
Arkar Hein <arkarhein14314@gmail.com>
2024-06-01 17:07:57 +0700

    commit leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let dividedByFour = year % 4;
+    let dividedByHundred = year % 100;
+    let dividedByFourHundred = year % 400;
+    
+    if (dividedByFour === 0 && (dividedByHundred !== 0 || dividedByFourHundred === 0)) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+console.log(leapYears(2000));
+console.log(leapYears(1985));
 
 // Do not edit below this line
 module.exports = leapYears;
