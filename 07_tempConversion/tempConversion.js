2cd09c225f2ce7885083d27ea399800bee728af0
Arkar Hein <arkarhein14314@gmail.com>
2024-06-02 00:41:47 +0700

    commit tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // fahrenheit to celsius equation
+  let celsius = (fahrenheit - 32) * (5 / 9)
+  return celsius; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  //celsius to fahrenheit equation
+  let fahrenheit = (celsius * 9/5) + 32;
+  return fahrenheit;
 };
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
